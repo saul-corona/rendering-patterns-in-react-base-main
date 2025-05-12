0ee1c853bf05dfef06631a543b47692d60cffccd
Saúl Corona <saulcoronascs@gmail.com>
2025-05-12 12:46:14 -0600

    add container presenter

--- src/components/ContainerPresenter/DataContainer.tsx
+++ src/components/ContainerPresenter/DataContainer.tsx
@@ -1,6 +1,46 @@
+import { useState, useEffect } from "react";
+
+import { DataPresenter } from "./DataPresenter";
+
+type DataItem = {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+};
+
 export const DataContainer = () => {
+  const [data, setData] = useState<DataItem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/data/data.json");
+  
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (e){
+        console.error(e);
+        setError(e as string);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [])
+  
   return (
     <div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      <DataPresenter data={data} />
     </div>
   );
 };
--- src/components/ContainerPresenter/DataPresenter.tsx
+++ src/components/ContainerPresenter/DataPresenter.tsx
@@ -1,6 +1,23 @@
-export const DataPresenter = () => {
-  return (
-    <>
-    </>
-  );
+import React from "react";
+
+type DataItem = {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
 };
+
+type DataPresenterProps = {
+  data: DataItem[];
+}
+
+export const DataPresenter: React.FC<DataPresenterProps> = ({ data }) => (
+  <>
+    {data.map((item) => (
+      <div key={item.id}>
+        <img src={item.image} alt={item.name} />
+        <em>{item.description}</em>
+      </div>
+    ))}
+  </>
+);
