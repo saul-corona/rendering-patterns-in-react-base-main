2eb0821eba16a5324e487a67b8222719ef2bb75f
Saúl Corona <saulcoronascs@gmail.com>
2025-05-12 12:14:22 -0600

    add customHook

--- src/components/CustomHooks/CustomHooks.tsx
+++ src/components/CustomHooks/CustomHooks.tsx
@@ -1,10 +1,33 @@
-const UserComponent = () => {
+import { useState } from "react";
+
+const useLoading = (initialLoading: boolean = false) => {
+  const [isLoading, setIsLoading] = useState(initialLoading);
+
+  return {
+    isLoading,
+    setIsLoading,
+  }
+}
+
+type UserComponentProps = {
+  name: string;
+}
+
+const UserComponent: React.FC<UserComponentProps> = ({ name }) => {
+  const { isLoading, setIsLoading } = useLoading(false);
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div>
+      <p>Hello 👋 {name}</p>
+      <button onClick={() => setIsLoading(true)}>simulate loading</button>
     </div>
   );
 };
 
 export const ParentComponent = () => {
-  return <UserComponent />;
+  return <UserComponent name="saul" />;
 };
