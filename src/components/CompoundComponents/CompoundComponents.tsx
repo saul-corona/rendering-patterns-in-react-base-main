17d2eaf884d97ab15454ada742db8e23b7721cf6
Saúl Corona <saulcoronascs@gmail.com>
2025-05-12 11:22:10 -0600

    add compound components

--- src/components/CompoundComponents/CompoundComponents.tsx
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -1,6 +1,18 @@
+import Tab from "./Tab";
+import Tabs from "./Tabs";
+
 export const CompoundComponents = () => {
   return (
-    <>
-    </>
+    <Tabs>
+      <Tab label="Lion">
+        <p>🦁</p>
+      </Tab>
+      <Tab label="Cat">
+        <p>🐱</p>
+      </Tab>
+      <Tab label="Dog">
+        <p>🐶</p>
+      </Tab>
+    </Tabs>
   );
 };
--- src/components/CompoundComponents/Tabs.tsx
+++ src/components/CompoundComponents/Tabs.tsx
@@ -1,8 +1,37 @@
+import React, { useState } from "react";
+
 import classes from "./CompoundComponents.module.css";
 
-const Tabs = () => {
+interface TabsProps {
+  children: React.ReactNode;
+};
+
+const Tabs: React.FC<TabsProps> = ({ children }) => {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  const handleTabClick = (index: number) => {
+    setActiveTabIndex(index);
+  }
+
+  const tabElements = React.Children.toArray(children).filter(
+    (child): child is React.ReactElement => React.isValidElement(child)
+  );
+
   return (
     <div className={classes.Tabs}>
+      <ul>
+        {tabElements.map((child, index) => (
+          <li key={index} 
+          className={`${index} === activeTabIndex ? classes.active : ""}`}
+          onClick={() => handleTabClick(index)}
+          >
+            {child.props.label}
+          </li>
+        ))}
+      </ul>
+      <p className={classes.TabContent}>
+        {tabElements[activeTabIndex].props.children}
+      </p>
     </div>
   );
 };
