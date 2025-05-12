c51b1a5d270583d9a9d4115abde9b97c820b67bb
Saúl Corona <saulcoronascs@gmail.com>
2025-05-12 12:57:41 -0600

    controlled and uncontroller components

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,17 @@
+import {useState} from "react";
+
 export const ControlledInput = () => {
+  const [value, setValue] = useState("");
+
   return (
     <div>
+      <input 
+        type="text"
+        value={value}
+        placeholder="Enter your name"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <p>Hello, {value}!</p>
     </div>
   );
 };
--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,22 @@
+import { useRef } from "react";
+
 export const UncontrolledInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if(inputRef.current) {
+    alert(`Product name: ${inputRef.current.value}`);
+    }
+  };
+
   return (
     <div>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Product name"
+      />
+      <button onClick={handleClick}>Add</button>
     </div>
   );
 };
